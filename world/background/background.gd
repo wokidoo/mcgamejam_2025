817e9b7a8c2264e52c2b55195baf08292142ce4e
CharlesP <charles.partous@gmail.com>
2025-01-11 18:10:04 -0500

    fix parallax, add player animation

--- world/background/background.gd
+++ world/background/background.gd
@@ -1,16 +1,11 @@
-extends CanvasLayer
+extends Parallax2D
 
-var material: ShaderMaterial
+var sprite_material: ShaderMaterial
 var player: Player
 var last_pos: Vector2 = Vector2.ZERO
 @export var shift_coefficient: float = 200;
+
 func _ready():
-	material = $ColorRect.material
+	sprite_material = $Sprite2D.material
 	player = get_tree().root.find_child("Player",true,false)
 	print_debug(player)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	var move_by = player.global_position - last_pos
-	last_pos = player.position
-	material.set_shader_parameter("player_pos",last_pos/shift_coefficient)
