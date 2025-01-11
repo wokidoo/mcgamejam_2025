1df1044fc63aedfd4397f501908aded145af380b
Charles Partous <charles.partous@gmail.com>
2025-01-11 14:17:05 -0500

    add background (#4)
    * add background
    
    * remove print statment
    
    * change background tex size

---
+++ world/background/background.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+var material: ShaderMaterial
+var player: Player
+var last_pos: Vector2 = Vector2.ZERO
+@export var shift_coefficient: float = 200;
+func _ready():
+	material = $ColorRect.material
+	player = get_tree().root.find_child("Player",true,false)
+	print_debug(player)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var move_by = player.global_position - last_pos
+	last_pos = player.position
+	material.set_shader_parameter("player_pos",last_pos/player.MAX_SPEED)
