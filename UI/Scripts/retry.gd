2195ab8e3d8eb9bca2de08744142216fc28732b8
Adam Farahat <farahatadam@gmail.com>
2025-01-12 00:14:17 -0500

    Implement Reset Button

--- UI/Scripts/retry.gd
+++ UI/Scripts/retry.gd
@@ -1,6 +1,5 @@
 extends Button
 
-var simultaneous_scene = preload("res://TestScenes/battle_arena_test.tscn").instantiate()
 
 func _on_pressed() -> void:
-	get_tree().root.add_child(simultaneous_scene)
+	get_tree().reload_current_scene()
