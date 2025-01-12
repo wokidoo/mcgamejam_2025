0b9df3a488e0e7bd4a93c8ebd677ec1dc723a93c
Adam Farahat <farahatadam@gmail.com>
2025-01-12 11:25:19 -0500

    Fixed Frozen Game Over to world_level

--- UI/game_over.gd
+++ UI/game_over.gd
@@ -10,3 +10,11 @@ func _game_over():
 	self.visible = true
 	get_tree().paused = true
 	player.queue_free()
+
+
+func _on_retry_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
