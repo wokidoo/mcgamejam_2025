e6088d245efdb789aa85b44ec1be4be831efa33e
hykhang126 <hykhang126@gmail.com>
2025-01-12 00:09:26 -0500

    Revert "Merge branch 'Adam' into Khang-level"
    This reverts commit 4edf47d61dd3b110c9d3f5aeb7135acc7c623ed8, reversing
    changes made to 4ae21474fe030fcc9b203b07b46d8708308c92f4.

--- UI/Scripts/retry.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-var simultaneous_scene = preload("res://TestScenes/battle_arena_test.tscn").instantiate()
-
-func _on_pressed() -> void:
-	get_tree().root.add_child(simultaneous_scene)
